8085d9392baaa82c7e5be296aaddedc0bcc045f2
Patrick Lima <patrickm.lima1@gmail.com>
2015-09-28 15:39:20 -0400

    basic server implementation (initial test)

---
+++ RemCalc/server/RemCalcServer.java
@@ -0,0 +1,19 @@
+package RemCalc.server;
+
+import java.rmi.*;
+import RemCalc.server.RemCalcImpl;
+public class RemCalcServer {
+	public static void main(String args[]) {
+		// sets the security manager
+		System.setSecurityManager(new RMISecurityManager());
+
+		try {
+			RemCalcImpl remCalcImpl = new RemCalcImpl();
+			Naming.rebind("RemCalcServer", remCalcImpl);
+			System.out.println("RemCalcServer is ready and waiting...\n");
+		} catch (Exception e) {
+			System.out.println("Error on Server");
+			e.printStackTrace();
+		}
+	}
+}
