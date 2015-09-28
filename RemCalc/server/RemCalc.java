a7762353c690bf5b75373466a011ffca5486e42b
Patrick Lima <patrickm.lima1@gmail.com>
2015-09-28 16:08:20 -0400

    add throws RemoteException to all methods in RemCalc interface

--- RemCalc/server/RemCalc.java
+++ RemCalc/server/RemCalc.java
@@ -1,7 +1,5 @@
-package RemCalc.server;
-
 import java.rmi.*;
 
 public interface RemCalc extends java.rmi.Remote {
-	public String calculate(String expr);
+	public String calculate(String expr) throws java.rmi.RemoteException;
 }
--- RemCalc/server/RemCalcImpl.java
+++ RemCalc/server/RemCalcImpl.java
@@ -1,16 +1,12 @@
-package RemCalc.server;
-
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
-import RemCalc.server.RemCalc;
-import RemCalc.server.ProcessExpression;
 
 public class RemCalcImpl extends UnicastRemoteObject implements RemCalc {
 	public RemCalcImpl() throws java.rmi.RemoteException {
 		super();
 	}
 
-	public String calculate(String expr) {
+	public String calculate(String expr) throws java.rmi.RemoteException {
 		return ProcessExpression.getInstance().eval(expr);
 	}
 }
